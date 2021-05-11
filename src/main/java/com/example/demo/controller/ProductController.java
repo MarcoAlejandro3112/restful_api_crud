4fcf1d2639ea80bcd7a84387a52c6895799d034f
Marco Altamirano <maaltamiranohiga@gmail.com>
2021-05-11 09:09:52 -0500

    Controller Product

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,46 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.ProductDto;
+import com.example.demo.model.Product;
+import com.example.demo.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService){
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<ProductDto> findAll() {
+        return this.productService.findAll();
+    }
+
+    @GetMapping("{id}")
+    public ProductDto findById(@PathVariable Long id) {
+        return this.productService.findById(id);
+    }
+
+    @PostMapping
+    public ProductDto save(@RequestBody ProductDto productDto){
+        productDto.setId(null);
+        return this.productService.save(productDto);
+    }
+
+    @PutMapping
+    public ProductDto update(@RequestBody ProductDto productDto){
+        return this.productService.save(productDto);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteById(@PathVariable Long id){
+        this.productService.deleteById(id);
+    }
+}
